4457e77f21a9a8c4264f3e7e91356134a5ea94ad
Jeferson Moraes <jefersonvilela72@gmail.com>
2026-04-12 16:17:11 -0300

    fix(budget): definitive fix for BudgetsPage JSX structure

--- src/pages/BudgetsPage.tsx
+++ src/pages/BudgetsPage.tsx
@@ -459,10 +459,11 @@ const BudgetsPage = () => {
                                 const newParams = new URLSearchParams(searchParams);
                                 newParams.delete('budgetId');
                                 setSearchParams(newParams, { replace: true });
-                        }
-                    }
-                />
-            )}
+                            }
+                        }}
+                    />
+                )
+            }
 
             {isAutomationModalOpen && selectedBudget && (
                 <BudgetAutomationModal
