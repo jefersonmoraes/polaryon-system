88f065be58a3f72481a55732cb38ef0ebbe14ad4
Jeferson Moraes <jefersonvilela72@gmail.com>
2026-05-15 14:00:11 -0300

    chore(deploy): release v3.6.30

--- electron/portal-preload.js
+++ electron/portal-preload.js
@@ -1,39 +1,10 @@
 (function() {
-    console.log("%c[POLARYON] Escuta-Geral v3.6.29 Ativado! 🛰️", "color: #00ff00; font-weight: bold;");
-
-    // 🕵️‍♂️ INTERCEPTOR DE FETCH (Moderno)
-    const originalFetch = window.fetch;
-    window.fetch = async (...args) => {
-        const response = await originalFetch(...args);
-        const url = args[0];
-        if (typeof url === 'string' && (url.includes('em-disputa') || url.includes('itens'))) {
-            const clone = response.clone();
-            clone.json().then(data => processSerproData(data, url)).catch(() => {});
-        }
-        return response;
-    };
-
-    // 🕵️‍♂️ INTERCEPTOR DE XHR (Tradicional - Usado pelo Comprasnet antigo)
-    const open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url) {
-        this._url = url;
-        return open.apply(this, arguments);
-    };
-
-    const send = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function() {
-        this.addEventListener('load', function() {
-            if (this._url && (this._url.includes('em-disputa') || this._url.includes('itens'))) {
-                try {
-                    const data = JSON.parse(this.responseText);
-                    processSerproData(data, this._url);
-                } catch (e) {}
-            }
-        });
-        return send.apply(this, arguments);
-    };
+    console.log("%c[POLARYON] Escuta-Geral v3.6.30 Ativado! 🛰️", "color: #00ff00; font-weight: bold;");
 
     function processSerproData(data, url) {
+        // Log de depuração para o usuário ver o que está passando
+        console.log(`%c[POLARYON] Interceptado: ${url.split('?')[0]}`, "color: #888; font-size: 10px;");
+
         const items = Array.isArray(data) ? data : (data.itens || []);
         const match = url.match(/\/compras\/(\d+)\//);
         const roomCode = match ? match[1] : null;
@@ -59,4 +30,36 @@
             console.log(`%c[POLARYON] Sincronia detectada na sala ${roomCode}! 📡`, "color: #00d4ff; font-weight: bold;");
         }
     }
+
+    // 🕵️‍♂️ INTERCEPTOR DE FETCH
+    const originalFetch = window.fetch;
+    window.fetch = async (...args) => {
+        const response = await originalFetch(...args);
+        const url = typeof args[0] === 'string' ? args[0] : args[0].url;
+        if (url.includes('serpro.gov.br')) {
+            const clone = response.clone();
+            clone.json().then(data => processSerproData(data, url)).catch(() => {});
+        }
+        return response;
+    };
+
+    // 🕵️‍♂️ INTERCEPTOR DE XHR
+    const open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+        this._url = url;
+        return open.apply(this, arguments);
+    };
+
+    const send = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function() {
+        this.addEventListener('load', function() {
+            if (this._url && this._url.includes('serpro.gov.br')) {
+                try {
+                    const data = JSON.parse(this.responseText);
+                    processSerproData(data, this._url);
+                } catch (e) {}
+            }
+        });
+        return send.apply(this, arguments);
+    };
 })();
