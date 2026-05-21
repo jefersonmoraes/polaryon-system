87171f184d87e784fce0c893d504f33315960c73
Jeferson Moraes <jefersonvilela72@gmail.com>
2026-05-21 18:01:51 -0300

    chore: bump version to 3.8.21

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -138,7 +138,7 @@ const AppSidebar = () => {
   };
 
 
-  const [appVersion, setAppVersion] = useState('3.8.20');
+  const [appVersion, setAppVersion] = useState('3.8.21');
 
   useEffect(() => {
     if (isElectron && (window as any).electronAPI) {
